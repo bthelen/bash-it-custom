4751ec7fa67ac7bb478f367f2c5310646f190ccd
Bruce E. Thelen <bthelen@pivotal.io>
2018-11-07 10:59:59 -0600

    Escape spaces in the history log file directory listing

--- log_history.bash
+++ log_history.bash
@@ -1,3 +1,3 @@
 # avoid clobbering other PROMPT_COMMANDs.
 echo $PROMPT_COMMAND | grep -q ".logs/bash-history"
-[ $? -gt 0 ] && PROMPT_COMMAND='if [ "$(id -u)" -ne 0 ]; then echo "$(date "+%Y-%m-%d.%H:%M:%S") $(pwd) $(history 1)" >> ~/.logs/bash-history-$(date "+%Y-%m-%d").log; fi;'"$PROMPT_COMMAND"
+[ $? -gt 0 ] && PROMPT_COMMAND='if [ "$(id -u)" -ne 0 ]; then echo "$(date "+%Y-%m-%d.%H:%M:%S") $(pwd | sed "s/ /+/g") $(history 1)" >> ~/.logs/bash-history-$(date "+%Y-%m-%d").log; fi;'"$PROMPT_COMMAND"
